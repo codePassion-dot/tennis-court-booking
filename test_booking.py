b242c36138767776a695b19f4ee1f4f00d2fa3d4
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2025-03-22 02:19:35 -0500

    dadf

--- test_booking.py
+++ test_booking.py
@@ -16,6 +16,7 @@ def run(playwright: Playwright) -> None:
     )
     context = browser.new_context(
         viewport={"width": 1920, "height": 1080},
+        screen={"width": 1920, "height": 1080},
         geolocation={"latitude": 4.60971, "longitude": -74.08175},
         permissions=["geolocation"],
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
